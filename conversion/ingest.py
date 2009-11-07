c1a2d91fee35bd30706b2cd2a1cf1a3dc0b44bb7
Matt Born <mattborn@bender.(none)>
2009-11-07 12:52:30 -0800

    Broken commit reflecting my attempt to segregate the logic of feeding the converter from actually performing conversions.

--- conversion/ingest.py
+++ conversion/ingest.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from __future__ import with_statement
+import optparse
 import os
 import subprocess
 import sys
@@ -8,22 +9,56 @@ import time
 
 import converters
 import database
+import sources.web
+import sources.file
 
 if __name__ == '__main__':
+    parser = optparse.OptionParser()
+    parser.add_options("-d", "--dir", dest = "dirs", \
+        help = "Convert all files in DIR", metavar = "DIR", action = "append")
+    parser.add_option("-f", "--file", dest = "files", \
+        help = "Convert FILE", metavar = "FILE", action = "append")
+    parser.add_option("--api", dest = "api", \
+        help = "Feed from the api at API", metavar = "API", action = "append")
+    parser.add_option("--service", dest = "service", \
+        help = "Run as a service", action="store_true", \
+        default=False)
+    (options, args) = parser.parse_args()
+
     converter_path = os.path.abspath('converters')
     products_path = os.path.abspath('products')
 
     converters = converters.list(path = converter_path)
 
-    for dataset in database.hopper():
-        for converter in converters:
-            if converter.accepts(object = dataset):
-                print '     + file converter located: ' + converter.name()
-                success = converter.process(object = dataset)
-                if success:
-                        print '     + file processed: ' + dataset.target()
-                        break
+    inputs = []
+    for path in (options.dirs + options.files):
+        input = None
+        if options.service:
+            input = sources.file.watcher(path = dir)
+        else:
+            input = sources.file.indexer(path = dir)
+        if input:
+            inputs.append(input)
+    for url in options.apis:
+        input = None
+        if options.service:
+            input = sources.web.watcher(path = url)
+        else:
+            input = sources.web.indexer(path = url)
+        if input:
+            inputs.append(input)
+
+    # Round-robin for now
+    for input in inputs:
+        for item in input.hopper():
+            for converter in converters:
+                if converter.accepts(object = dataset):
+                    print '     + file converter located: ' + converter.name()
+                    success = converter.process(object = dataset)
+                    if success:
+                            print '     + file processed: ' + dataset.target()
+                            break
+                    else:
+                        print '     - file processing failed'
                 else:
-                    print '     - file processing failed'
-            else:
-                print '     - file has no converter'
+                    print '     - file has no converter'
---
+++ conversion/sources/web.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+
+api_root = 'http://api.del.icio.us'
+
+import hashlib
+import os
+import time
+import urllib2
+
+# Queries given directory at given refresh interval (expressed in seconds)
+# If the file's modification time is different than the last time we saw
+# it, or if this is the first time we have seen it, yield the filename.
+def watch(path, refresh = 10.0):
+    modtimes = dict()
+    print ' + monitoring: ' + path
+    while True:
+        cycle = time.time()
+        if os.path.exists(path):
+            present = os.listdir(path)
+            present = [os.path.join(path, file) for file in present]
+            missing = [key for key in modtimes.keys() if key not in present]
+            for file in missing:
+                print '   - file missing: ' + file
+                del modtimes[file]
+            for file in present:
+                modtime = os.path.getmtime(file)
+                if not modtimes.has_key(file) or modtimes[file] != modtime:
+                    modtimes[file] = modtime
+                    print '   + file noticed: ' + file
+                    yield file
+        else:
+            modtimes.clear()
+        remaining = time.time() - cycle
+        if refresh > remaining:
+            time.sleep(refresh - remaining)
+
+def hash(path):
+    if os.path.isfile(path):
+        file = open(path)
+        checksum = hashlib.md5()
+        while True:
+            data = file.read(2**20)
+            if data:
+                checksum.update(data)
+            else:
+                break
+        file.close()
+        return checksum.hexdigest()
+
+class entry:
+    def __init__(self, unprocessed):
+        self._unprocessed = unprocessed
+        self._processed = os.path.join(os.path.abspath('products'), 
+            os.path.splitext(os.path.basename(self._unprocessed))[0] + '.xml')
+        self._md5 = hash(self._unprocessed)
+    def __enter__(self):
+        pass
+    def __exit__(self, type, value, traceback):
+        pass
+    def filename(self):
+        return self._unprocessed
+    def target(self):
+        return self._processed
+    def hash(self):
+        return self._md5
+
+class metadata(dict):
+    def __init__(self):
+        dict.__init__(self)
+    def _read(self):
+        # Syncronize the cached version in memory with the version on disk
+        # if necessary.  We don't really know who else might be manipulating
+        # the database at the same time, and in fact we can't really protect
+        # ourselves from a truly bad actor, but by adhering to this simple
+        # locking mechanism we can protect ourselves from conflicts with
+        # other unknown programs which manipulate or reference the database.
+        pass
+    def _write(self):
+        # Same as _read, but in the opposite direction.  Due to our locking
+        # mechanism, we do not behave nicely if the dictionary has changed
+        # while we hold the lock.  If you don't respect our lock we feel
+        # no need to respect your changes.
+        pass
+    def _lock(self):
+        pass
+    def _unlock(self):
+        pass
+    def has_key(self, key):
+        if self._lock():
+            ret = dict.has_key(self, key)
+            self._unlock()
+            return ret
+        else:
+            return False
+    def __getitem__(self, key):
+        if self._lock():
+            ret = dict.__getitem__(self, key)
+            self._unlock()
+            return ret
+        else:
+            return None
+    def __setitem__(self, key, value):
+        if self._lock():
+            dict.__setitem__(self, key, value)
+            self._unlock()
+    def __delitem__(self, key):
+        if self._lock():
+            dict.__delitem__(self, key)
+            self._unlock()
+#    def keys(self):
+#        print 'keys'
+#        return dict.keys(self)
+
+def hopper():
+    watcher = watch(path = os.path.abspath('hopper'))
+    hashes = metadata()
+#        try:
+#            return urllib2.urlopen(api_root).read()
+#        except urllib2.HTTPError, e:
+#            print 'HTTP error ' + str(e.code) + ': ' + e.reason.args[1]
+#        except urllib2.URLError, e:
+#            print 'Network connection error: ' + e.reason.args[1]
+    while True:
+        file = watcher.next()
+        dataset = entry(unprocessed = file)
+        if not dataset.hash() in hashes:
+            print '     + file hash calculated: ' + str(dataset.hash())
+            hashes[dataset.hash()] = dataset
+            yield dataset
+        else:
+            print '     - file already processed'
+
+if __name__ == '__main__':
+    print next()
